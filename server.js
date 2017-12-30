f2a521a775c7009ff310830f1a5819d4fe84a6f9
Jesus Coloyan <wahoothemighty@yahoo.com>
2017-12-29 19:50:14 -0800

    set up files

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/", {
+	useMongoClient: true
+});
+
